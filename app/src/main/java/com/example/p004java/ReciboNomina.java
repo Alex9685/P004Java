b5b113bca6e8659f29319cfbc916992f1250ee0a
Tlacoyo Emplumado <alex928280@gmail.com>
2023-05-29 21:05:30 -0700

    Creación del proyecto

---
+++ app/src/main/java/com/example/p004java/ReciboNomina.java
@@ -0,0 +1,21 @@
+package com.example.p004java;
+
+import android.widget.TextView;
+
+public class ReciboNomina {
+
+        private int numRecibo;
+        private String nombre;
+        private int horasTrabNormal;
+        private int horasTrabExtras;
+        private int puesto;
+
+    public ReciboNomina(int numRecibo, String nombre, int horasTrabNormal, int horasTrabExtras, int puesto) {
+        this.numRecibo = numRecibo;
+
+
+
+
+
+
+
